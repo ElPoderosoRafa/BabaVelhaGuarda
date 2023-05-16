79564181af26eb372a3aadb4d30c96a8d1178223
unknown <rafaeloliveira.programador@gmail.com>
2023-05-15 22:44:04 -0300

    Primeiro commit sistema Web Baba Velha Guarda

---
+++ Models/VelhaGuardaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabaVelhaGuardaDigital.Models
+{
+    public class VelhaGuardaContext : DbContext
+    {
+        public VelhaGuardaContext(DbContextOptions<VelhaGuardaContext> options) : base(options)
+        {
+        }
+  
+
+    }
+}
